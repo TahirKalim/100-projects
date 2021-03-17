796e7fb89de6f9a03ff773a8a6c7170bd3d7ef1b
tahir kalim <thrkalim@gmail.com>
2021-03-17 14:59:18 +0100

    github profile part 1

---
+++ Github Profiles/script.js
@@ -0,0 +1,16 @@
+const APIURL = 'https://api.github.com/users/'
+
+const form = document.getElementById('form')
+
+async function getUser(username){
+    try{
+        const {data} = await axios(APIURL + username)
+        console.log(data)
+    }catch(err){
+        console.log(err)
+    }
+}
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const user = search.value
+})
